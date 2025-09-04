9a4850a7faa67cd183e90c1e7752db98608e4291
jordanvest <vestjc1@gmail.com>
2025-09-04 11:00:37 -1000

    2025-09-04 commit

---
+++ Week_02/Scripts/Week_02_script.R
@@ -0,0 +1,9 @@
+### This is my first script. I am learning how to import data
+### Created by: Jordan Vest
+### Created on: 2025-09-04
+#################################################
+### Load Libraries ##########
+library(tidyverse)
+library(here)
+### Read in data ###
+WeightData<-read_csv(here("Week_02","Data","weightdata.csv"))
