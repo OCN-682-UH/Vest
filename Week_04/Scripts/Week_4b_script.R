fb8546a84e7f3280fa9180393b75071ed947a4c4
jordanvest <vestjc1@gmail.com>
2025-09-18 13:57:09 -1000

    organized

---
+++ Week_04/Scripts/Week_4b_script.R
@@ -0,0 +1,8 @@
+###Create a new clean script
+Remove all the NAs
+Separate the Tide_time column into appropriate columns for analysis
+Filter out a subset of data (your choice)
+use either pivot_longer or pivot_wider at least once
+Calculate some summary statistics (can be anything) and export the csv file into the output folder
+Make any kind of plot (it cannot be a boxplot) and export it into the output folder
+Make sure you comment your code and your data, outputs, and script are in the appropriate folders
